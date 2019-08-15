8e34adfb7aceb4458a11331453bfd698cf522dc9
Victoria Argon <vkkizilova@gmail.com>
2019-08-15 18:11:58 +0300

    AbstractException created

--- src/io/wonko/xo/model/Field.java
+++ src/io/wonko/xo/model/Field.java
@@ -5,12 +5,13 @@ import java.awt.*;
 public class Field {
 
     private static final int FIELD_SIZE = 3;
+    private static final int MIN_COORDINATE= 0;
+    private static final int MAX_COORDINATE= FIELD_SIZE;
 
     private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
 
     public int getSize() {
         return FIELD_SIZE; }
-
 
      public Figure getFigure(Point point) {
         return field[point.x][point.y];
@@ -18,5 +19,13 @@ public class Field {
 
      public void setFigure(final Point point, final Figure figure) {
         field[point.x][point.y] = figure;
+     }
+
+     private boolean checkPoint(final Point point){
+        return checkCoordinate(point.x) && checkCoordinate(point.y);
+     }
+
+     private boolean checkCoordinate(final int coordinate){
+        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
      }
 }
