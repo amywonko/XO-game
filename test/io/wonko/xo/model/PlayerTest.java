ea155bfb152186de6c7ff50233a480471e2277f9
Victoria Argon <vkkizilova@gmail.com>
2019-08-15 17:36:42 +0300

    Test created

---
+++ test/io/wonko/xo/model/PlayerTest.java
@@ -0,0 +1,32 @@
+package io.wonko.xo.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Test
+    public void getName() {
+        final String inputValue = "Argon";
+        final String expectedValue = inputValue;
+
+        final Player player = new Player(inputValue, null);
+
+        final String actualValue = player.getName();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void getFigure() {
+        final Figure inputValue = Figure.X;
+        final Figure expectedValue = inputValue;
+
+        final Player player = new Player(null, inputValue);
+
+        final Figure actualValue = player.getFigure();
+
+        assertEquals(expectedValue, actualValue);
+    }
+}
