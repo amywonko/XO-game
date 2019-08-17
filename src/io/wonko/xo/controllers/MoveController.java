ce90a2af09e507cfa9d5034b7ac05706e0a850d0
Victoria Argon <vkkizilova@gmail.com>
2019-08-17 12:42:53 +0300

    Initial MoveController

--- src/io/wonko/xo/controllers/MoveController.java
+++ src/io/wonko/xo/controllers/MoveController.java
@@ -1,7 +1,22 @@
 package io.wonko.xo.controllers;
 
-public class MoveController {
-    public void applyFigure(){
+import io.wonko.xo.model.Field;
+import io.wonko.xo.model.Figure;
+import io.wonko.xo.model.exceptions.AlreadyOccupiedException;
+import io.wonko.xo.model.exceptions.InvalidPointException;
 
+import java.awt.*;
+
+public class MoveController {
+
+    public void applyFigure(final Field field,
+                            final Point point,
+                            final Figure figure) throws AlreadyOccupiedException,
+                                                        InvalidPointException {
+        if(field.getFigure(point)!= null){
+            throw new AlreadyOccupiedException();
+        }
+
+        field.setFigure(point, figure);
     }
 }
